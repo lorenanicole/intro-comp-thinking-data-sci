9363b66950e48999e744b069e644887ac2184ff8
Lorena Mesa <lorenamesa@Lorenas-MacBook-Pro.local>
2015-12-21 06:58:35 -0600

    Lect 9 Prob 2

---
+++ week5/lect9_prob2.py
@@ -0,0 +1,23 @@
+from lect9_graph import *
+
+'''
+Add the appropriate edges to the graph.
+'''
+nodes = []
+nodes.append(Node("ABC")) # nodes[0]
+nodes.append(Node("ACB")) # nodes[1]
+nodes.append(Node("BAC")) # nodes[2]
+nodes.append(Node("BCA")) # nodes[3]
+nodes.append(Node("CAB")) # nodes[4]
+nodes.append(Node("CBA")) # nodes[5]
+
+g = Graph()
+for n in nodes:
+    g.addNode(n)
+
+g.addEdge(Edge(nodes[0], nodes[1]))
+g.addEdge(Edge(nodes[0], nodes[2]))
+g.addEdge(Edge(nodes[1], nodes[4]))
+g.addEdge(Edge(nodes[2], nodes[3]))
+g.addEdge(Edge(nodes[3], nodes[5]))
+g.addEdge(Edge(nodes[4], nodes[5]))
